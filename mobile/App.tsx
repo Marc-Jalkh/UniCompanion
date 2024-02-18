a192ce8e12fe64e270c9bb879cb896b9160fa90c
Marc-Jalkh <marcjalkh.mej@gmail.com>
2024-02-18 17:22:38 +0200

    changed app.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Launch from './src/Launch';
+import {ColorThemeProvider} from './src/Common/utils/theme/ColorThemeContex';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <ColorThemeProvider>
       <Launch />
-    </SafeAreaView>
+    </ColorThemeProvider>
   );
 }
 
